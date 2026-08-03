a4bff60268e3125fc94a86cc7daa5dd6bb2817d2
ludmila tawane rodrigues dos anjos <ludmila.tawane@escolar.ifrn.edu.br>
2026-08-03 11:26:48 -0300

    efwq

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,8 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrl: './home.css'
 })
-export class Home {}
+export class Home {
+
+  selectedProfile = 'pai';
+
+  selectProfile(profile: string) {
+    this.selectedProfile = profile;
+  }
+
+}
