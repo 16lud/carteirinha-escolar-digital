68e09ed29071e18fb08d0cc39ac13ed335db11d4
ludmila tawane rodrigues dos anjos <ludmila.tawane@escolar.ifrn.edu.br>
2026-08-10 16:09:48 -0300

    rj

---
+++ src/app/pages/login-escola/login-escola.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-escola',
+  imports: [],
+  templateUrl: './login-escola.html',
+  styleUrl: './login-escola.css',
+})
+export class LoginEscola {}
