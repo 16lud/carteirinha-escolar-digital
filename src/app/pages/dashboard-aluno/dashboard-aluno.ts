8df0b13749a2daffac3deac8ad80dc1593ab86be
ludmila tawane rodrigues dos anjos <ludmila.tawane@escolar.ifrn.edu.br>
2026-08-10 17:24:37 -0300

    fg

--- src/app/pages/dashboard-aluno/dashboard-aluno.ts
+++ src/app/pages/dashboard-aluno/dashboard-aluno.ts
@@ -1,54 +1,62 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 import { QRCodeComponent } from 'angularx-qrcode';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-aluno',
-  standalone: true,
   imports: [QRCodeComponent],
   templateUrl: './dashboard-aluno.html',
   styleUrl: './dashboard-aluno.css'
 })
 export class DashboardAluno {
 
-  // Dados fictícios para testar a interface
   nomeAluno = 'Ludmila Tawane';
   matricula = '2026001234';
-  turma = '3º Informática';
+  turma = '3º INFO';
   turno = 'Matutino';
   escola = 'IFRN';
 
-  // Dados que serão transformados em QR Code
-  // Futuramente isso poderá vir do cadastro/login.
   qrCodeData = 'TRANSPORTE-DIGITAL-ALUNO-2026001234';
+
 
   constructor(private router: Router) {}
 
+
   irParaInicio() {
-    // Mantém o aluno na página inicial do dashboard
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
   }
+
 
   minhaRota() {
     console.log('Minha rota');
   }
 
+
   localizacao() {
     console.log('Localização');
   }
+
 
   horarios() {
     console.log('Horários');
   }
 
+
   notificacoes() {
     console.log('Notificações');
   }
+
 
   meuPerfil() {
     console.log('Meu perfil');
   }
 
+
   sair() {
-    this.router.navigate(['/login']);
+    this.router.navigate(['/']);
   }
+
 }
