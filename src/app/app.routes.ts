218629af8e570ec052e8ddcd073a0da95093fc84
ludmila tawane rodrigues dos anjos <ludmila.tawane@escolar.ifrn.edu.br>
2026-08-10 16:16:16 -0300

    tykd

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,27 +8,35 @@ import { CadastroAluno } from './pages/cadastro-aluno/cadastro-aluno';
 import { CadastroMotorista } from './pages/cadastro-motorista/cadastro-motorista';
 import { CadastroEscola } from './pages/cadastro-escola/cadastro-escola';
 
+import { LoginAluno } from './pages/login-aluno/login-aluno';
+import { LoginMotorista } from './pages/login-motorista/login-motorista';
+import { LoginEscola } from './pages/login-escola/login-escola';
+
 import { DashboardAluno } from './pages/dashboard-aluno/dashboard-aluno';
 import { DashboardMotorista } from './pages/dashboard-motorista/dashboard-motorista';
 import { DashboardEscola } from './pages/dashboard-escola/dashboard-escola';
 
 export const routes: Routes = [
 
+  // HOME
   {
     path: '',
     component: Home
   },
 
+  // LOGIN PRINCIPAL
   {
     path: 'login',
     component: Login
   },
 
+  // ESCOLHA DO PERFIL PARA CADASTRO
   {
     path: 'escolha-perfil',
     component: EscolhaPerfil
   },
 
+  // CADASTROS
   {
     path: 'cadastro-aluno',
     component: CadastroAluno
@@ -44,6 +52,23 @@ export const routes: Routes = [
     component: CadastroEscola
   },
 
+  // LOGINS ESPECÍFICOS
+  {
+    path: 'login-aluno',
+    component: LoginAluno
+  },
+
+  {
+    path: 'login-motorista',
+    component: LoginMotorista
+  },
+
+  {
+    path: 'login-escola',
+    component: LoginEscola
+  },
+
+  // DASHBOARDS
   {
     path: 'dashboard-aluno',
     component: DashboardAluno
