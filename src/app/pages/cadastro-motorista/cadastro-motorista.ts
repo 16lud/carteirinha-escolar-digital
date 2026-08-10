351691b1070c2516c20cdd2027d848f658007baa
ludmila tawane rodrigues dos anjos <ludmila.tawane@escolar.ifrn.edu.br>
2026-08-10 16:45:18 -0300

    ther

--- src/app/pages/cadastro-motorista/cadastro-motorista.ts
+++ src/app/pages/cadastro-motorista/cadastro-motorista.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro-motorista',
@@ -6,4 +7,17 @@ import { Component } from '@angular/core';
   templateUrl: './cadastro-motorista.html',
   styleUrl: './cadastro-motorista.css',
 })
-export class CadastroMotorista {}
+export class CadastroMotorista {
+
+  constructor(private router: Router) {}
+
+  cadastrar() {
+    alert('Cadastro realizado com sucesso!');
+
+    this.router.navigate(['/dashboard-motorista']);
+  }
+
+  voltar() {
+    this.router.navigate(['/escolha-perfil']);
+  }
+}
