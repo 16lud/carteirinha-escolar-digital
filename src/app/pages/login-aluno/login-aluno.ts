e7ebe947dba7e1d2cf8f51c7bd79b99fe4078450
ludmila tawane rodrigues dos anjos <ludmila.tawane@escolar.ifrn.edu.br>
2026-08-10 16:12:38 -0300

    svd

--- src/app/pages/login-aluno/login-aluno.ts
+++ src/app/pages/login-aluno/login-aluno.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-aluno',
   imports: [],
   templateUrl: './login-aluno.html',
-  styleUrl: './login-aluno.css',
+  styleUrl: './login-aluno.css'
 })
-export class LoginAluno {}
+export class LoginAluno {
+
+  constructor(private router: Router) {}
+
+  entrar() {
+    this.router.navigate(['/dashboard-aluno']);
+  }
+
+  criarConta() {
+    this.router.navigate(['/cadastro-aluno']);
+  }
+
+  voltar() {
+    this.router.navigate(['/login']);
+  }
+
+}
