1361d570d591fbc0630285a02eeecd946f27bc85
ludmila tawane rodrigues dos anjos <ludmila.tawane@escolar.ifrn.edu.br>
2026-08-10 16:22:00 -0300

    dshf

--- src/app/pages/login-escola/login-escola.ts
+++ src/app/pages/login-escola/login-escola.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-escola',
   imports: [],
   templateUrl: './login-escola.html',
-  styleUrl: './login-escola.css',
+  styleUrl: './login-escola.css'
 })
-export class LoginEscola {}
+export class LoginEscola {
+
+  constructor(private router: Router) {}
+
+  entrar() {
+    this.router.navigate(['/dashboard-escola']);
+  }
+
+  voltar() {
+    this.router.navigate(['/login']);
+  }
+
+  criarConta() {
+    this.router.navigate(['/cadastro-escola']);
+  }
+}
--- src/app/pages/login-motorista/login-motorista.ts
+++ src/app/pages/login-motorista/login-motorista.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-motorista',
   imports: [],
   templateUrl: './login-motorista.html',
-  styleUrl: './login-motorista.css',
+  styleUrl: './login-motorista.css'
 })
-export class LoginMotorista {}
+export class LoginMotorista {
+
+  constructor(private router: Router) {}
+
+  entrar() {
+    this.router.navigate(['/dashboard-motorista']);
+  }
+
+  criarConta() {
+    this.router.navigate(['/cadastro-motorista']);
+  }
+
+  voltar() {
+    this.router.navigate(['/login']);
+  }
+
+  voltarHome() {
+    this.router.navigate(['/']);
+  }
+}
